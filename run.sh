440c27882dc90cbc9db4d45cbd5b6429b94946b8
softwarecheng <softwarecheng@126.com>
2024-09-17 16:29:48 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/release/electrs -vvvv --cookie jacky:_RZekaGRgKQJSIOYi6vq0_CkJtjoCootamy81J2cDn0 --db-dir ./data --daemon-rpc-addr "192.168.10.188:8337" \
+target/release/electrs -vvvv --cookie jacky:_RZekaGRgKQJSIOYi6vq0_CkJtjoCootamy81J2cDn0 --db-dir ./data --daemon-rpc-host "127.0.0.1:8337" \
     --network mainnet --daemon-cert-path ./rpc1.cert --jsonrpc-import --cors "*" --http-addr "0.0.0.0:3002"
