5e19cecbe10a40f21f596ab9d650cb2a989554d3
softwarecheng <softwarecheng@126.com>
2025-03-07 23:29:40 +0800

    fix

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@
 # satstestnet
 git checkout satsnet
 argo build --release;
- /data/github/satsnet-mempool-electrs/target/release/electrs -vvvv --cookie q17AIoqBJSEhW7djqjn0nTsZcz4=:nnlkAZn58bqsyYwVtHIajZ16cj8= --db-dir ./db --daemon-rpc-host "192.168.10.103:19527" \
-    --network satstestnet --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables --cors "*" --http-addr "0.0.0.0:3000" --jsonrpc-import
+target/release/electrs -vvvv --cookie q17AIoqBJSEhW7djqjn0nTsZcz4=:nnlkAZn58bqsyYwVtHIajZ16cj8= --db-dir ./db --daemon-rpc-host "192.168.10.103:19527" \
+    --monitoring-addr 127.0.0.1:44224 --electrum-rpc-addr 127.0.0.1:60501 \
+    --network satstestnet --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables --cors "*" --http-addr "0.0.0.0:3001" --jsonrpc-import
 
 
