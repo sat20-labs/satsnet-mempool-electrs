8cec07b1e19c5fafcc4310e47f05dea4d8be8d70
softwarecheng <softwarecheng@126.com>
2025-03-07 22:58:55 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-target/release/electrs -vvvv --cookie jacky:_RZekaGRgKQJSIOYi6vq0_CkJtjoCootamy81J2cDn0 --db-dir ./data --daemon-rpc-host "192.168.10.103:19527" \
-    --network mainnet --jsonrpc-import --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables \ --cors "*" --http-addr "0.0.0.0:3000"
+#target/release/electrs -vvvv --cookie jacky:_RZekaGRgKQJSIOYi6vq0_CkJtjoCootamy81J2cDn0 --db-dir ./data --daemon-rpc-host "192.168.10.103:19527" \
+ #   --network mainnet --jsonrpc-import --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables \ --cors "*" --http-addr "0.0.0.0:3000"
 
-target/release/electrs -vvvv --cookie q17AIoqBJSEhW7djqjn0nTsZcz4=:nnlkAZn58bqsyYwVtHIajZ16cj8= --db-dir ./db --daemon-rpc-host "192.168.10.103:19527" --network satstestnet --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables --cors "*" --http-addr "0.0.0.0:3000" --jsonrpc-import
+# satstestnet
+git checkout satsnet
+argo build --release;
+target/release/electrs -vvvv --cookie q17AIoqBJSEhW7djqjn0nTsZcz4=:nnlkAZn58bqsyYwVtHIajZ16cj8= --db-dir ./db --daemon-rpc-host "192.168.10.103:19527" \
+    --network satstestnet --utxos-limit 5000 --electrum-txs-limit 5000 --address-search --index-unspendables --cors "*" --http-addr "0.0.0.0:3000" --jsonrpc-import
 
 
